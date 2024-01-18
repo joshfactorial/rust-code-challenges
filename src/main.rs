0e4c54896a8b0214b39ae4f644a89a96500eeb2c
Joshua Allen <jallen17@illinois.edu>
2024-01-18 13:22:09 -0600

    Solution to print anything, but it isn't perfect

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-fn info(a: &T) {
-    todo!();
+fn info<T: std::fmt::Debug + ?Sized>(a: &T) {
+    println!("{:?}", a);
 }
 
 fn main() {
@@ -9,15 +9,15 @@ fn main() {
     info(&b);
 
     // Advanced 1
-    // use std::ffi::CString;
+    use std::ffi::CString;
     
-    // let c = CString::new("?").unwrap();
-    // info(&input);
+    let c = CString::new("?").unwrap();
+    info(&c);
 
     // Advanced 2
-    // use std::path::Path;
-    // let d = Path::new("/tmp/linkedin-learning");
-    // info(d);
+    use std::path::Path;
+    let d = Path::new("/tmp/linkedin-learning");
+    info(d);
 }
 
 
@@ -33,22 +33,22 @@ fn string() {
     info(&input);
 }
 
-// #[test]
-// fn chars() {
-//     let input = 'r';
-//     info(&input);
-// }
+#[test]
+fn chars() {
+    let input = 'r';
+    info(&input);
+}
 
-// #[test]
-// fn cstring() {
-//     use std::ffi::{CString};
-//     let input = CString::new("Rust").unwrap();
-//     info(&input);
-// }
+#[test]
+fn cstring() {
+    use std::ffi::{CString};
+    let input = CString::new("Rust").unwrap();
+    info(&input);
+}
 
-// #[test]
-// fn path() {
-//     use std::path::Path;
-//     let input = Path::new("/tmp/rust");
-//     info(input);
-// }
+#[test]
+fn path() {
+    use std::path::Path;
+    let input = Path::new("/tmp/rust");
+    info(input);
+}
