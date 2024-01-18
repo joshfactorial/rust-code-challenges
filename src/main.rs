035fcaa276c31b42c8cc8f3da7d5e7b8921f7676
Joshua Allen <jallen17@illinois.edu>
2024-01-18 16:22:05 -0600

    My solution was less elegant than the official solution, but it works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[allow(dead_code)]
+
 #[derive(Debug, PartialEq)]
 enum Card {
     Ace,
@@ -30,8 +32,39 @@ impl Hand {
     }
 
     fn value(&self) -> usize {
-        // TODO: implement this method
-        0 
+        let mut hand_value: usize = 0;
+        let mut aces_seen = 0;
+
+        for card in &self.cards {
+            hand_value += match *card {
+                Card::King | Card::Queen | Card::Jack => 10,
+                Card::Nine => 9,
+                Card::Eight => 8,
+                Card::Seven => 7,
+                Card::Six => 6,
+                Card::Five => 5,
+                Card::Four => 4,
+                Card::Three => 3,
+                Card::Two => 2,
+                Card::Ace => {
+                    aces_seen += 1;
+                    11
+                },
+            }
+        }
+
+        if aces_seen > 0 {
+            // We have an ace and we are over 21, so the ace switches value from 11 to 1
+            if hand_value > 21 {
+                hand_value - (10 * aces_seen)
+            // otherwise the ace stays an 11
+            } else {
+                hand_value
+            }
+        // No ace, so you get what you get
+        } else {
+            hand_value
+        }
     }
 
     fn is_loosing_hand(&self) -> bool {
@@ -68,8 +101,9 @@ fn risky_hand() {
     hand.add(Card::King);
     hand.add(Card::Queen);
     hand.add(Card::Ace);
+    hand.add(Card::Ace);
     
-    assert_eq!(hand.value(), 21);
+    assert_eq!(hand.value(), 22);
 }
 
 #[test]
@@ -77,8 +111,9 @@ fn oops() {
     let mut hand = Hand::new();
     hand.add(Card::King);
     hand.add(Card::Seven);
+    hand.add(Card::Ace);
     hand.add(Card::Five);
     
     assert!(hand.is_loosing_hand());
-    assert_eq!(hand.value(), 22);
+    assert_eq!(hand.value(), 23);
 }
