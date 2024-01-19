56ba0973a26f1ebfc695f9a1bc29c972f5a242a9
Joshua Allen <jallen17@illinois.edu>
2024-01-19 12:58:01 -0600

    My answer was, in the end, pretty much identical to the official one

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+use chrono::NaiveDate;
+
 fn weeks_between(a: &str, b: &str) -> i32 {
-    todo!()
+
+    // mine
+    let a_date = NaiveDate::parse_from_str(a, "%Y-%m-%d");
+    let b_date = NaiveDate::parse_from_str(b, "%Y-%m-%d");
+
+    let elapsed_days = b_date.unwrap() - a_date.unwrap();
+    (elapsed_days.num_days() / 7) as i32
+
 }
 
 fn main() {
@@ -24,4 +33,10 @@ fn one_week() {
 fn past() {
     let n_weeks = weeks_between("1010-10-18", "1010-10-10");
     assert_eq!(n_weeks, -1);
+}
+
+#[test]
+fn way_past() {
+    let n_weeks = weeks_between("2023-01-01", "2024-01-03");
+    assert_eq!(n_weeks, 52)
 }
