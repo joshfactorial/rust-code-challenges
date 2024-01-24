1e1bc88f3c71fe4da7110a9541a1c111663979bd
Joshua Allen <jallen17@illinois.edu>
2024-01-24 09:42:43 -0600

    I needed help figuring out the error stuff, but the main logic I got

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,74 @@
 use std::str::FromStr;
 
+#[derive(PartialEq)]
+#[derive(Debug)]
 struct Isbn {
     raw: String,
     digits: Vec<u8>,
 }
 
+#[derive(Debug)]
+#[derive(PartialEq)]
+enum InvalidIsbn {
+    TooLong,
+    TooShort,
+    FailedChecksum,
+    InvalidCharacter(usize, char),
+}
+
 impl FromStr for Isbn {
-    type Err = (); // TODO: replace with appropriate type
+    type Err = InvalidIsbn;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        todo!();        
+        // his version
+        let mut digits = Vec::with_capacity(15);
+        for (i, c) in s.char_indices() {
+            match c {
+                '-' => continue,
+                '0'..='9' => digits.push(c.to_digit(10).unwrap() as u8),
+                _ => { return Err(InvalidIsbn::InvalidCharacter(i, c)); },
+            }
+        }
+
+        let n_digits = digits.len();
+        if n_digits > 13 {
+            return Err(InvalidIsbn::TooLong);
+        } else if n_digits < 13 {
+            return Err(InvalidIsbn::TooShort);
+        }
+
+        if digits[12] != calculate_check_digit(&digits) {
+            return Err(InvalidIsbn::FailedChecksum);
+        }
+
+        Ok(Isbn { raw: s.to_string(), digits: digits })
+
+        // my version
+        // let trimmed_s = s.trim_matches('-');
+        // let mut digits = "".to_string();
+        // for character in trimmed_s.chars() {
+        //     if character.is_numeric() {
+        //         // string version
+        //         digits = digits + &character.to_string();
+        //
+        //     }
+        // }
+        //
+        // // I thought the instructions said to drop the last zero if it ends in zero, but it looked like he was assuming we'd always drop the last digit.
+        // // looks like the final digit of the ISBN is the check digit.
+        // let (isbn_string, checksum_string) = digits.split_at(digits.len() - 1);
+        // let checksum = checksum_string.parse::<u8>().unwrap();
+        // let mut isbn_digits: Vec<u8> = vec![];
+        // for char in isbn_string.chars() {
+        //     isbn_digits.push(char.to_string().parse::<u8>().unwrap())
+        // };
+        //
+        // if calculate_check_digit(&isbn_digits) != checksum {
+        //     FailedChecksum
+        // } else {
+        //     Ok(Self { raw: s.to_string(), digits: isbn_digits })
+        // }
+
     }
 }
 
@@ -21,13 +80,40 @@ impl std::fmt::Display for Isbn {
 
 // https://en.wikipedia.org/wiki/International_Standard_Book_Number#ISBN-13_check_digit_calculation
 fn calculate_check_digit(digits: &[u8]) -> u8 {
-    todo!()
+    // my version. I used a simpler array and modulo arithmetic to select the weight.
+    let weights = [1, 3];
+    let mut weights_sum = 0;
+    for index in 0..digits.len() {
+        weights_sum += digits[index] * weights[index % 2]
+    }
+    let check = 10 - (weights_sum % 10);
+    if check == 10 {
+        0
+    } else {
+        check
+    }
+
+    // This is the official version. The iterator and zipping  makes sense, but my version is slightly more compact
+    // const WEIGHTS: [u8; 12] = [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3];
+    //
+    // let weight_applied: u32 = digits
+    //     .iter()
+    //     .zip(WEIGHTS.iter())
+    //     .map(|(&x, &y)| x * y)
+    //     .map(|subtotal| subtotal as u32)
+    //     .sum();
+    //
+    // let check_digit = 10 - (weight_applied % 10);
+    // match check_digit {
+    //     10 => 0_u8,
+    //     x => x as u8,
+    // }
 }
 
 fn main() {
     let rust_in_action: Isbn = "978-3-16-148410-0".parse().unwrap();
 
-    println!("Rust in Action's ISBN-13 ({})is valid!", rust_in_action);
+    println!("Rust in Action's ISBN-13 ({}) is valid!", rust_in_action);
 }
 
 #[test]
@@ -47,4 +133,10 @@ fn can_correctly_calculate_check_digits() {
 #[test]
 fn rust_in_action() {
     let _: Isbn = "978-3-16-148410-0".parse().unwrap();
+}
+
+#[test]
+fn bad_isbn() {
+    let check: Result<Isbn, InvalidIsbn> = Isbn::from_str("78-3-16-148410-0");
+    assert_eq!(check, Err(InvalidIsbn::TooShort));
 }
