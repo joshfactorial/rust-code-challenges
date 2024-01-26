d213bfac6ef6d617aeea17f922e000eed97b2ebe
Joshua Allen <jallen17@illinois.edu>
2024-01-26 09:50:05 -0600

    I used the FromHex function. It basically worked, but the errors weren't helpful

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::fmt::Display;
 use std::str::FromStr;
+use core::str;
 
 #[derive(Debug, PartialEq)]
-struct Rgb; // TODO: design data structure
+struct Rgb {
+    red: u8,
+    blue: u8,
+    green: u8,
+}
 
 trait RgbChannels {
     fn r(&self) -> u8;
@@ -13,11 +18,54 @@ trait RgbChannels {
 }
 
 impl RgbChannels for Rgb {
-    // TODO: implement trait
+    fn r(&self) -> u8 {
+        self.red
+    }
+
+    fn g(&self) -> u8 {
+        self.green
+    }
+
+    fn b(&self) -> u8 {
+        self.blue
+    }
+}
+
+#[derive(Debug)]
+enum ParseColorError {
+    RedChannelOutOfBounds,
+    GreenChannelOutOfBounds,
+    BlueChannelOutOfBounds,
+    NoLeadingHash,
 }
 
 impl FromStr for Rgb {
-    // TODO: implement trait
+
+    type Err = ParseColorError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // their solution. Gets the error involved.
+        if let Some(hex_only) = s.strip_prefix('#') {
+            let r = u8::from_str_radix(&hex_only[0..2], 16)
+                                          .or_else(|_| Err(ParseColorError::RedChannelOutOfBounds))?;
+            let g = u8::from_str_radix(&hex_only[2..4], 16)
+                                        .or_else(|_| Err(ParseColorError::GreenChannelOutOfBounds))?;
+            let b = u8::from_str_radix(&hex_only[4..6], 16)
+                .or_else(|_| Err(ParseColorError::BlueChannelOutOfBounds))?;
+            Ok(Rgb { red: r, green: g, blue: b })
+        } else {
+            return Err(ParseColorError::NoLeadingHash);
+        }
+
+        // my original solution
+        // didn't make use of channel out of bounds errors, but still passed the tests.
+        // if !s.starts_with('#') {
+        //     return Err(ParseColorError::NoLeadingHash);
+        // }
+        // let hex_only = s.trim_start_matches('#');
+        // let buffer = <[u8; 3]>::from_hex(hex_only).unwrap();
+        // Ok(Rgb { red: buffer[0], green: buffer[1], blue: buffer[2] })
+
+    }
 }
 
 impl Display for Rgb {
